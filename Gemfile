182fd983350542003a7c6570514af3db6382406e
yuusukeshann <ihate-gomikusokasuugliness@ezweb.ne.jp>
2022-06-16 00:21:10 +0900

    新規登録機能の実装

--- Gemfile
+++ Gemfile
@@ -43,7 +43,6 @@ group :development do
   gem 'rubocop', require: false
 end
 
-
 group :test do
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '>= 2.15'
@@ -54,3 +53,4 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem 'devise'
---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname,             presence: true
+  validates :family_name,          presence: true
+  validates :first_name,           presence: true
+  validates :family_name_kana,     presence: true
+  validates :first_name_kana,      presence: true
+  validates :birthday,             presence: true
+         
+  has_many :items
+  has_many :purchase_records
+end
