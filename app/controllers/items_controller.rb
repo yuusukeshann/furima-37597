25ce89454ecc23365a5c7c9293f6408a3ef5e8d6
遠川悠輔 <tookawayuusuke@tookawayuusukenoMacBook-Pro.local>
2022-06-25 11:29:30 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,13 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def message_params
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  end
+
+
+
 end
