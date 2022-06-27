a7aeee9a1c77170b62b05a7827faa13ce32455b2
遠川悠輔 <tookawayuusuke@tookawayuusukenoMacBook-Pro.local>
2022-06-27 15:22:54 +0900

    バリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,9 @@ class Item < ApplicationRecord
   validates :delivery_charge_id,   presence: true,  numericality: { other_than: 1 , message: "can't be blank" }
   validates :prefecture_id,        presence: true,  numericality: { other_than: 1 , message: "can't be blank" }
   validates :days_to_ship_id,      presence: true,  numericality: { other_than: 1 , message: "can't be blank" }
-  validates :price,                presence: true
+  with_options  presence: true,  format: { with: /\A[0-9]+\z/, message: '半角数字を使用してください' } do
+  validates :price,  numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
   
    # belongs_to :item
   #  has_one :purchase_record
