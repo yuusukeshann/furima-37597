db7a954ef6648df69dc8dc1f1e88787bd606e6a4
遠川悠輔 <tookawayuusuke@tookawayuusukenoMacBook-Pro.local>
2022-06-30 15:44:23 +0900

    商品情報の登録

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,6 +6,20 @@ def new
   @item = Item.new
 end
 
+def create
+  @item = Item.new(item_params)
+  if @item.save
+    redirect_to root_path
+  else
+    render :new
+  end
+end
 
+  private
+
+  def item_params
+    params.require(:item).permit(:image,:brand_name,:expository_writing,:category_id,:condition_id,:delivery_charge_id,:prefecture_id,:days_to_ship_id,:price ).merge(user_id: current_user.id)
+
+end
 
 end
