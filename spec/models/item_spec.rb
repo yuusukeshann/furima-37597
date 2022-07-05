a164845ade56558f5977dd38834b8116abe0a749
遠川悠輔 <tookawayuusuke@tookawayuusukenoMacBook-Pro.local>
2022-07-05 13:29:09 +0900

    「再修正」商品出品機能

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,62 +12,62 @@ RSpec.describe Item, type: :model do
       end
     end
     context '商品が出品できない場合' do
-      it 'imageが空では出品できない' do
+      it '画像が空では出品できない' do
         @item.image = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
-      it 'brand_nameが空では出品できない' do
+      it '商品名が空では出品できない' do
         @item.brand_name = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Brand name can't be blank")
       end
-      it 'expository_writingが空では出品できない' do
+      it '商品の説明が空では出品できない' do
         @item.expository_writing = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Expository writing can't be blank")
       end
-      it 'categoryが空では出品できない' do
-        @item.category_id = ''
+      it 'カテゴリーに「---」が選択されている場合は出品できない' do
+        @item.category_id = ' 1 '
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
-      it 'conditionが空では出品できない' do
-        @item.condition_id = ''
+      it '商品の状態に「---」が選択されている場合は出品できない' do
+        @item.condition_id = ' 1 '
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
-      it 'delivery_chargeが空では出品できない' do
-        @item.delivery_charge_id = ''
+      it '配送料の負担に「---」が選択されている場合は出品できない' do
+        @item.delivery_charge_id = ' 1 '
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery charge can't be blank")
       end
-      it 'prefectureが空では出品できない' do
-        @item.prefecture_id = ''
+      it '発送元の地域に「---」が選択されている場合は出品できない' do
+        @item.prefecture_id = ' 1 '
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
-      it 'days_to_shipが空では出品できない' do
-        @item.days_to_ship_id = ''
+      it '発送までの日数に「---」が選択されている場合は出品できない' do
+        @item.days_to_ship_id = ' 1 '
         @item.valid?
         expect(@item.errors.full_messages).to include("Days to ship can't be blank")
       end
-      it 'priceが空では出品できない' do
+      it '販売価格が空では出品できない' do
         @item.price = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
-      it 'priceが¥300以上でないと出品できない' do
+      it '販売価格が¥300以上でないと出品できない' do
         @item.price = '200'
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
       end
-      it 'priceが¥9,999,999を超えると出品できない' do
+      it '販売価格が¥9,999,999を超えると出品できない' do
         @item.price = '10000000'
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
-      it 'priceが半角数値以外だと出品できない' do
+      it '販売価格が半角数値以外だと出品できない' do
         @item.price = '１０００'
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not a number")
