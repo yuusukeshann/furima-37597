313e6e1c4233c52e68d130018781474c74d11abd
yuusukeshann <ihate-gomikusokasuugliness@ezweb.ne.jp>
2022-07-15 13:18:46 +0900

    編集ページの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,6 +23,13 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  
+
+
   private
 
   def item_params
