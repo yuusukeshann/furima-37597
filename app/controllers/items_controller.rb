a217ea20494fdccde10b4fb7356138b5844db661
yuusukeshann <ihate-gomikusokasuugliness@ezweb.ne.jp>
2022-07-16 16:33:34 +0900

    「修正」商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, except: [:index, :show]
-
+  before_action :set_item, only: [:edit, :show, :update]
   def index
     @items = Item.order('created_at DESC')
   end
@@ -20,15 +20,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    # メモ @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
+    # メモ @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
+    # メモ @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to action: :show
     else
@@ -47,4 +47,10 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
     redirect_to action: :index unless user_signed_in? && current_user.id == @item.user_id
   end
+
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
