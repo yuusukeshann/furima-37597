a5e09a7acb2884862fb95def655cd62ce3dfa81f
yuusukeshann <ihate-gomikusokasuugliness@ezweb.ne.jp>
2022-07-16 14:34:44 +0900

    編集情報の更新、編集ページへの遷移の制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:edit, :new]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @items = Item.order('created_at DESC')
@@ -27,7 +28,14 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to action: :show
+    else
+      render :edit
+    end
+  end
 
 
   private
@@ -36,4 +44,13 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :brand_name, :expository_writing, :category_id, :condition_id, :delivery_charge_id,
                                  :prefecture_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
+  end
+
+
 end
