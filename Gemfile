61c9f8c776b787c38ecc0514a26b2405abd17f25
yuusukeshann <ihate-gomikusokasuugliness@ezweb.ne.jp>
2022-08-27 15:08:17 +0900

    修正

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ group :development, :test do
   gem 'factory_bot_rails'
   gem 'faker'
   gem 'gimei'
+  gem 'faker-japanese'
 end
 
 group :development do
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,7 @@ class User < ApplicationRecord
 
   validates :nickname,             presence: true
   validates :birthday,             presence: true
-  # memo validates :family_name,          presence: true
-  # memo validates :first_name,           presence: true
-  # memo validates :family_name_kana,     presence: true
-  # memo validates :first_name_kana,      presence: true
-
+ 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :family_name
     validates :first_name
